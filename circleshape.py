61f85baa25edee144804fc0cc3cc779d902b7d2e
Sponge <spongedingus@gmail.com>
2024-09-02 12:39:30 -0500

    Inital commit of Asteroids game

---
+++ circleshape.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, 'white', self.triangle(), 2)
+
+    def update(self, dt):
+        
+        pass
+
+    def collision(self, other_circle):
+        if self.position.distance_to(other_circle.position) <= self.radius + other_circle.radius:
+            return True
+        else:
+            return False
---
+++ test_pygame.py
@@ -0,0 +1,2 @@
+import pygame
+print("Pygame is installed and working!")
